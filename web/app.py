3f5720ba18945af8c3330fc61fa78004ecc19b8c
Jordan Smith <jsmith37@testium.cs.uoregon.edu>
2021-04-21 16:42:43 -0700

    Added functions to properly handle file requests

--- web/app.py
+++ web/app.py
@@ -1,14 +1,27 @@
 """
-John Doe's Flask API.
+Jordan Smith's Flask API.
 """
 
-from flask import Flask
+from flask import Flaski, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "UOCIS docker demo!\n"
+
+@app.route("/<request>")
+def return_page(request):
+    return render_template(f"{request}")
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def file_forbidden(e):
+    return render_template('403.html'), 403
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
