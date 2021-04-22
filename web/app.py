807208730c28ce598c3c9aefcb4cfcc1fe5024fc
Jordan Smith <jsmith37@testium.cs.uoregon.edu>
2021-04-21 20:02:18 -0700

    Updated the request handling to handle 404 and 403 errors

--- web/app.py
+++ web/app.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
+docroot = "templates/"
+
 
 @app.route("/")
 def hello():
@@ -13,8 +15,19 @@ def hello():
 
 @app.route("/<request>")
 def return_page(request):
-    return render_template(f"{request}")
+    if ('~' in request or '..' in request):
+        return file_forbidden()
+    elif (os.path.exists(docroot + request)):
+        return render_template(f"{request}")
+    else:
+        return not_found()
 
+def not_found():
+    return render_template('404.html'), 404
+
+def file_forbidden():
+    return render_template('403.html'), 403
+"""
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html'), 404
@@ -22,6 +35,7 @@ def not_found(e):
 @app.errorhandler(403)
 def file_forbidden(e):
     return render_template('403.html'), 403
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
