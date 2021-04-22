109b0381314706679c33948e6077b1c4025d3383
Jordan Smith <jsmith37@testium.cs.uoregon.edu>
2021-04-22 12:02:11 -0700

    Gets docroot from credentials file, uses send_from_directory for non-template pages

--- web/app.py
+++ web/app.py
@@ -2,32 +2,32 @@
 Jordan Smith's Flask API.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, send_from_directory
 import os
+import configparser
 
 app = Flask(__name__)
-docroot = "templates/"
 
+# Load the docroot from credentials file
+config = configparser.ConfigParser()
+config.read("credentials.ini")
+docroot = config["DEFAULT"]["DOCROOT"]
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
-
-@app.route("/<request>")
+@app.route("/<path:request>")
 def return_page(request):
+    # If the request has a bad character => 403
     if ('~' in request or '..' in request):
-        return file_forbidden()
+        abort(403)
+
+    # If the request exists in the docroot => Return the requested page
     elif (os.path.exists(docroot + request)):
-        return render_template(f"{request}")
+        return send_from_directory(docroot, request)
+        # return render_template(f"{request}")
+
+    # Otherwise, the file doesn't exist => 404
     else:
-        return not_found()
+        abort(404)
 
-def not_found():
-    return render_template('404.html'), 404
-
-def file_forbidden():
-    return render_template('403.html'), 403
-"""
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html'), 404
@@ -35,7 +35,7 @@ def not_found(e):
 @app.errorhandler(403)
 def file_forbidden(e):
     return render_template('403.html'), 403
-"""
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
